7f7c4af7397c5206010066e64d72ec2ceda6e034
William Kennedy <alexkenned@gmail.com>
2020-02-25 21:14:04 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -3,9 +3,14 @@ require 'directors_database'
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
-  # Change the code below to pretty print the nds with pp
-  nil
+  pp directors_database
 end
 
 def print_first_directors_movie_titles
+  director_index=0
+  title_index=0 
+  while director_index < directors_database[0].length do
+    pp directors_database[0][title_index]
+    title_index+=1 
+  end
 end
